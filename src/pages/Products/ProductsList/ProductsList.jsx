e83156354336c1125827207f73d1d855aefa1d38
DamianCenco <damian.cancela@cencosud.com>
2022-10-05 18:20:52 -0300

    Agrego productList

--- src/pages/Products/ProductsList/ProductsList.jsx
+++ src/pages/Products/ProductsList/ProductsList.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getProducts } from "../../../utils/apiConfig";
+import style from "./productsList.module.css";
+import arrow from "../../../assets/chevron-right (1).svg";
 
 const ProductsList = () => {
-  return (
-    <div>ProductsList</div>
-  )
-}
+  const [products, setProducts] = useState({});
 
-export default ProductsList
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await getProducts();
+      console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <ul className={style.productList}>
+        {products.length &&
+          products.map((product) => (
+            <Link key={product.id} to={`/products/${product.id}`}>
+              <li className={style.product}>
+                <div>
+                  <img src={product.images[product.images.length - 1]} alt="" />
+                  <div className={style.productDetails}>
+                    <p className={style.productTitle}>{product.title}</p>
+                    <p className={style.productDescription}>#{product.id}</p>
+                  </div>
+                </div>
+                <img className={style.arrow} src={arrow} alt="" />
+              </li>
+            </Link>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductsList;
