62cce25c12d3b5a5b23183503bd7ec250b9b24ef
nicolas-berlin <nicolas.berlin@cencosud.com>
2022-10-06 17:34:45 -0300

    arreglando pagina de producto

--- src/pages/Products/ProductView/ProductView.jsx
+++ src/pages/Products/ProductView/ProductView.jsx
@@ -1,16 +1,31 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import useForm from "../../../hooks/useForm";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import useForm from "../../../hooks/dataForm";
 import axios from "axios";
+import { getProducts } from "../../../utils/apiConfig";
 import style from "./productView.module.css";
 
 const ProductView = () => {
-  const useAxios = () => {
-    const url = "http://localhost:5000/api/product/1";
-    axios.get(url, {}).then(function (response) {
-      console.log(response);
-    });
+  const id = useParams().id;
+  const showProduct = () => {
+    return axios
+      .get(`http://localhost:5000/api/product/${id}`, {})
+      .then(({ data }) => {
+        console.log(data);
+        return JSON.stringify(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
+
+  const [productOnPage, setProductOnPage] = useState("");
+
+  useEffect(() => {
+    showProduct().then((randomData) => {
+      setProductOnPage(randomData || "No existe el producto.");
+    });
+  }, []);
 
   const navigate = useNavigate();
   const [image, setImage] = useState([]);
@@ -32,7 +47,6 @@ const ProductView = () => {
   const { data, handleChange } = useForm(initialValues);
 
   const handleSubmit = async (e) => {
-    console.log(data);
     e.preventDefault();
     try {
       const response = await axios.put(
@@ -43,97 +57,84 @@ const ProductView = () => {
         navigate("/products");
       }
     } catch (error) {
-      console.log(error);
       setWarning("ERR0R");
     }
   };
 
   return (
-    <>
-      <div className={style.container}>
-        <div className={style.products}>
-          <h2>Titulo</h2>
-          <img alt="product"></img>
-          <h4>5000</h4>
-          <p>Puntos</p>
-          <h4>1000</h4>
-          <p>Stock</p>
-        </div>
-        <h2 className={style.headings}>Información</h2>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="nombre">Nombre</label>
-          <br />
-          <input
-            className={style.inputs}
-            type="text"
-            name="nombre"
-            placeholder="Ingresar Nombre"
-            onChange={handleChange}
-          />
-          <br />
-          <label htmlFor="calor">Valor</label>
-          <br />
-          <input
-            className={style.inputs}
-            type="number"
-            name="valor"
-            placeholder="Ingresar Valor"
-            onChange={handleChange}
-          />
-          <br />
-          <label htmlFor="stock">Stock</label>
-          <br />
-          <input
-            className={style.inputs}
-            type="number"
-            name="stock"
-            placeholder="Ingresar Cantidad"
-            onChange={handleChange}
-          />
-          <div>
-            <label htmlFor="descripcion">Descripción</label>
-            <br />
-            <input
-              className={style.description}
-              type="text"
-              name="descripcion"
-              placeholder="Ingresar Descripción"
-              onChange={handleChange}
-            />
-            <br />
-            <br />
-            <label htmlFor="tienda">Tienda</label>
-            <br />
-            <select
-              className={style.inputs}
-              type="select"
-              name="tienda"
-              placeholder="Seleccionar"
-              onChange={handleChange}
-            />
-          </div>
-          <h3 className={style.headings}>Galería de Imágenes</h3>
-          <div>
-            <label htmlFor="imagen">Nueva Imagen</label>
-            <input
-              type="file"
-              name="imagen"
-              accept="image/*"
-              value={image}
-              onChange={(e) => setImage(e.target.value)}
-            />
-          </div>
-          <div>
-            <h4 className={style.headings}>Imágenes Actuales</h4>
-          </div>
-          <div>
-            <button type="submit">Guardar</button>
-            <button onClick={useAxios}>Cancelar</button>
-            <div>{warning && <p>{warning}</p>}</div>
-          </div>
-        </form>
+    <div className={style.container}>
+      <div>{productOnPage}</div>
+      <div className={style.products}>
+        <h2>Titulo</h2>
+        <img alt="product"></img>
+        <h4>5000</h4>
+        <p>Puntos</p>
+        <h4>1000</h4>
+        <p>Stock</p>
       </div>
-    </>
+      <h2 className={style.headings}>Información</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="nombre">Nombre</label>
+        <br />
+        <input
+          className={style.inputs}
+          type="text"
+          name="nombre"
+          placeholder="Ingresar Nombre"
+          onChange={handleChange}
+        />
+        <br />
+        <label htmlFor="calor">Valor</label>
+        <br />
+        <input
+          className={style.inputs}
+          type="number"
+          name="valor"
+          placeholder="Ingresar Valor"
+          onChange={handleChange}
+        />
+        <br />
+        <label htmlFor="stock">Stock</label>
+        <br />
+        <input
+          className={style.inputs}
+          type="number"
+          name="stock"
+          placeholder="Ingresar Cantidad"
+          onChange={handleChange}
+        />
+        <div>
+          <label htmlFor="descripcion">Descripción</label>
+          <br />
+          <input
+            className={style.description}
+            type="text"
+            name="descripcion"
+            placeholder="Ingresar Descripción"
+            onChange={handleChange}
+          />
+          <br />
+          <label htmlFor="tienda">Tienda</label>
+          <br />
+          <select
+            className={style.inputs}
+            type="select"
+            name="tienda"
+            placeholder="Seleccionar"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <h4 className={style.headings}>Imágenes Actuales</h4>
+        </div>
+        <div>
+          <button type="submit">Guardar</button>
+          <button>Cancelar</button>
+          <div>{warning && <p>{warning}</p>}</div>
+        </div>
+      </form>
+    </div>
   );
 };
 
