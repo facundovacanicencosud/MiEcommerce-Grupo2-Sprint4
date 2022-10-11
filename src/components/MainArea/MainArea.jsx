d73bc45e9bc8f35d94b39200a4930e3ea8e5b73d
facundovacanicencosud <111792420+facundovacanicencosud@users.noreply.github.com>
2022-10-11 15:43:51 -0300

    agregando modo dark

--- src/components/MainArea/MainArea.jsx
+++ src/components/MainArea/MainArea.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useContext } from 'react'
 import { Outlet } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
 import Navbar from '../Navbar/Navbar'
 
 const MainArea = () => {
+  const {theme} = useContext(AppContext);
   return (
-    <main className='main-area'>
+    <main className={`main-area ${theme?"dark":""}`}>
         <Navbar />
         <section>
             <Outlet />
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -5,12 +5,16 @@ export const AppContext = createContext();
 export const Contexto = ({ children }) => {
     const [activeSidebar, setActiveSidebar] = useState(false);
     const [searchQuery, setSearchQuery] = useState("");
+    const [theme, setTheme] = useState(false);
+
 
   const value = {
     activeSidebar,
     setActiveSidebar,
     searchQuery,
-    setSearchQuery
+    setSearchQuery,
+    theme,
+    setTheme
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
